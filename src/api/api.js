0cc15fe773c7811b05c2255d5e9aaaa0795d25da
Om Gupta <68691016+OmGuptaIND@users.noreply.github.com>
2021-11-08 16:32:50 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 import { SERVER_LINK } from '../Utils/constants';
 
-
-// const hostLink = 'https://vbeasbackend.herokuapp.com';
-// const link = 'http://localhost:8000/api/';
 export default axios.create({
     baseURL: `${SERVER_LINK}/api/`,
     headers: { 'Content-Type': 'application/json' },
